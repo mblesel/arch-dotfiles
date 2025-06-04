f9cede52ac10c1125aa89df14ee3a7f6dad30ff1
Michael Blesel <michael.blesel@ovgu.de>
2025-06-04 21:55:27 +0200

    fix for markdown foldexpr

--- nvim/.config/nvim/lua/mblesel/markdown_funcs.lua
+++ nvim/.config/nvim/lua/mblesel/markdown_funcs.lua
@@ -68,11 +68,13 @@ function _G.markdown_foldexpr()
     end
     -- own hack to have empty lines between closed h2 folds
     -- can't figure out how this could be accomplished for other foldlevels as well
-    local line2 = vim.fn.getline(lnum +1)
+    local line2 = vim.fn.getline(lnum + 1)
     local empty = line:match("^$")
     local heading2 = line2:match("^(#+)%s")
-    if empty and heading2 and #heading2 == 2 then
-        return ">0"
+    if empty and heading2 then
+        if #heading == 2 then
+            return ">0"
+        end
     else
         return "="
     end
