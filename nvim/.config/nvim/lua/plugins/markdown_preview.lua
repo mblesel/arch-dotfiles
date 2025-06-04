63400fb97af1b461539902bceedccae862ade1c2
Michael Blesel <michael.blesel@ovgu.de>
2025-06-04 21:26:49 +0200

    markdown-preview style changes

--- nvim/.config/nvim/lua/plugins/markdown_preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown_preview.lua
@@ -11,5 +11,7 @@ return {
     },
     config = function ()
         vim.g.mkdp_markdown_css = os.getenv("HOME") .. "/.config/nvim/markdown_styles/retro.css"
+        vim.g.mkdp_highlight_css = os.getenv("HOME") .. "/.config/nvim/markdown_styles/retro-highlight.css"
+
     end,
 }
