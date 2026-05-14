4ff4b4502c05b342fec22facf21b716d8bcc670e
Michael Blesel <michael.blesel@ovgu.de>
2026-05-14 21:22:18 +0200

    small fix

--- nvim/.config/nvim/lua/plugins/markdown-plus.lua
+++ nvim/.config/nvim/lua/plugins/markdown-plus.lua
@@ -1,68 +1,66 @@
 return {
     "yousefhadder/markdown-plus.nvim",
     ft = "markdown",
-    config = function()
-        require("markdown-plus").setup({
+    opts = {
+        enabled = true,
+
+        features = {
+            list_management = true,
+            text_formatting = true,
+            code_block = true,
+            links = true,
+            quotes = true,
+            callouts = true,
+            table = true,
+            footnotes = true,
+            html_block_awareness = true,
+            -- disabled
+            images = false,
+            thematic_break = false,
+            headers_toc = false,
+        },
+
+        keymaps = {
             enabled = true,
+        },
 
-            features = {
-                list_management = true,
-                text_formatting = true,
-                code_block = true,
-                links = true,
-                quotes = true,
-                callouts = true,
-                table = true,
-                footnotes = true,
-                html_block_awareness = true,
-                -- disabled
-                images = false,
-                thematic_break = false,
-                headers_toc = false,
+        filetypes = { "markdown" },
+
+        list = {
+            smart_outdent = true,
+            checkbox_completion = {
+                enabled = false,
             },
+        },
 
-            keymaps = {
+        callouts = {
+            default_type = "NOTE",
+            custom_types = {},
+        },
+
+        code_block = {
+            enabled = true,
+            fence_style = "backtick", -- "backtick" | "tilde"
+            languages = { "lua", "python", "javascript", "typescript", "bash", "json", "yaml", "markdown" },
+        },
+
+        table = {
+            enabled = true,
+            auto_format = true,
+            default_alignment = "left",
+            confirm_destructive = true,
+        },
+
+        footnotes = {
+            section_header = "Footnotes",
+            confirm_delete = true,
+        },
+
+        links = {
+            smart_paste = {
                 enabled = true,
+                timeout = 5, -- 1..30
             },
-
-            filetypes = { "markdown" },
-
-            list = {
-                smart_outdent = true,
-                checkbox_completion = {
-                    enabled = false,
-                },
-            },
-
-            callouts = {
-                default_type = "NOTE",
-                custom_types = {},
-            },
-
-            code_block = {
-                enabled = true,
-                fence_style = "backtick", -- "backtick" | "tilde"
-                languages = { "lua", "python", "javascript", "typescript", "bash", "json", "yaml", "markdown" },
-            },
-
-            table = {
-                enabled = true,
-                auto_format = true,
-                default_alignment = "left",
-                confirm_destructive = true,
-            },
-
-            footnotes = {
-                section_header = "Footnotes",
-                confirm_delete = true,
-            },
-
-            links = {
-                smart_paste = {
-                    enabled = true,
-                    timeout = 5, -- 1..30
-                },
-            },
-        })
-    end,
+        },
+    },
 }
